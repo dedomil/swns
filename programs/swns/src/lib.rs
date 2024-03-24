f016a97adf7c0a5e8098872ff5dc5df58cd6a09d
dedomil <dedomil@tuta.io>
2024-03-24 14:20:37 +0530

    add function to update records to web service account

--- programs/swns/src/lib.rs
+++ programs/swns/src/lib.rs
@@ -13,14 +13,36 @@ pub mod swns {
         let web_service_account = &mut ctx.accounts.web_service_account;
 
         // todo: add valid domain checks
-        if domain.chars().count() < 1 {
-            return err!(Errors::InvalidDomain);
-        }
+        // using require_gt, domain length should be strictly greater than 0
+        require_gt!(domain.chars().count(), 0, WebServiceError::InvalidDomain);
 
         web_service_account.authority = ctx.accounts.authority.key();
         web_service_account.receiver = ctx.accounts.receiver.key();
         web_service_account.domain = domain;
 
+        Ok(())
+    }
+
+    pub fn update_web_service_account(
+        ctx: Context<UpdateWebServiceAccount>,
+        domain: String,
+    ) -> Result<()> {
+        let web_service_account = &mut ctx.accounts.web_service_account;
+
+        // todo: add valid domain checks
+        require_gt!(domain.chars().count(), 0);
+
+        // check whether the signer (authority) is the one who created the web_service_account
+        // if this check isn't there anyone can sign the tx and update web_service_account
+        require_keys_eq!(
+            web_service_account.authority.key(),
+            ctx.accounts.authority.key(),
+            WebServiceError::AuthorityMismatch
+        );
+
+        // update the fields, i.e. reciever and domain
+        web_service_account.receiver = ctx.accounts.receiver.key();
+        web_service_account.domain = domain;
         Ok(())
     }
 }
@@ -41,6 +63,25 @@ pub struct CreateWebServiceAccount<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(domain: String)]
+pub struct UpdateWebServiceAccount<'info> {
+    #[account(
+        mut,
+        has_one = authority,
+        realloc = 8 + 32 + 32 + 4 + domain.len(),
+        realloc::payer = authority,
+        realloc::zero = false
+    )]
+    pub web_service_account: Account<'info, WebServiceAccount>,
+    /// CHECK: any type
+    pub receiver: AccountInfo<'info>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    // system program is required to realloc space
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 pub struct WebServiceAccount {
     pub receiver: Pubkey,
@@ -49,7 +90,9 @@ pub struct WebServiceAccount {
 }
 
 #[error_code]
-pub enum Errors {
+pub enum WebServiceError {
     #[msg("Invalid domain specified.")]
     InvalidDomain,
+    #[msg("Unauthorized, authority mismatch")]
+    AuthorityMismatch,
 }
